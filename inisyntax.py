646ecefad6e0a110a294b7217d4b76b89003e398
ozsolarwind <ozsolarwind@users.noreply.github.com>
2016-05-24 13:55:29 +0800

    Add files via upload

---
+++ inisyntax.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+#
+#  Copyright (C) 2015 Sustainable Energy Now Inc., Angus King     
+#
+#  inisyntax.py - This file is part of SIREN.
+#
+#  SIREN is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as 
+#  published by the Free Software Foundation, either version 3 of 
+#  the License, or (at your option) any later version.
+#
+#  SIREN is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General 
+#  Public License along with SIREN.  If not, see
+#  <http://www.gnu.org/licenses/>.
+#
+
+import sys
+
+from PyQt4.QtCore import QRegExp
+from PyQt4.QtGui import QColor, QTextCharFormat, QFont, QSyntaxHighlighter
+
+def format(color, style=''):
+    """Return a QTextCharFormat with the given attributes.
+    """
+    _color = QColor()
+    _color.setNamedColor(color)
+
+    _format = QTextCharFormat()
+    _format.setForeground(_color)
+    if 'bold' in style:
+        _format.setFontWeight(QFont.Bold)
+    if 'italic' in style:
+        _format.setFontItalic(True)
+
+    return _format
+
+
+# Syntax styles that can be shared by all languages
+STYLES = {
+    'operator': format('darkGreen'),
+    'group': format('darkMagenta'),
+    'comment': format('darkBlue'),
+    'self': format('black'),
+}
+
+
+class IniHighlighter (QSyntaxHighlighter):
+    """Syntax highlighter for the .ini files.
+    """
+    comment = ['^\;', '^#']
+    group = ['^\[']
+    operators = ['\=']
+
+    def __init__(self, document):
+        QSyntaxHighlighter.__init__(self, document)
+        rules = []
+        rules += [(r'%s' % c, 0, STYLES['comment'])
+            for c in self.comment]
+        rules += [(r'%s' % g, 0, STYLES['group'])
+            for g in self.group]
+        rules += [(r'%s' % o, 0, STYLES['operator'])
+           for o in self.operators]
+        # Build a QRegExp for each pattern
+        self.rules = [(QRegExp(pat), index, fmt)
+            for (pat, index, fmt) in rules]
+
+    def highlightBlock(self, text):
+        """Apply syntax highlighting to the given block of text.
+        """
+        # Do other syntax formatting
+        for expression, nth, format in self.rules:
+            index = expression.indexIn(text, 0)
+            if index == 0:
+                self.setFormat(index, len(text), format)
+                break
+            if index > 0:
+                self.setFormat(0, index, format)
+                break
+        self.setCurrentBlockState(0)
---
+++ senuser.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+#
+#  Copyright (C) 2015 Sustainable Energy Now Inc., Angus King     
+#
+#  senuser.py - This file is part of SIREN.
+#
+#  SIREN is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as 
+#  published by the Free Software Foundation, either version 3 of 
+#  the License, or (at your option) any later version.
+#
+#  SIREN is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General 
+#  Public License along with SIREN.  If not, see
+#  <http://www.gnu.org/licenses/>.
+#
+
+#
+# return current userid
+
+import os
+try:
+    import pwd
+except:
+    pass
+import sys
+
+def getUser():
+    if sys.platform == 'win32' or sys.platform == 'cygwin': # windows
+        return os.environ.get("USERNAME")
+
+    elif sys.platform == 'darwin': # osx64
+        return os.environ.get("USERNAME")
+
+    elif sys.platform == 'linux2': # linux
+        return pwd.getpwuid(os.geteuid()).pw_name
+    else:
+        return os.environ.get("USERNAME")
