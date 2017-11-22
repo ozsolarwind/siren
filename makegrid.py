ef5622da4f9b04be5cec22c0fd38e3b9792d21e3
ozsolarwind <angus@ozsolarwind.com>
2017-11-22 19:56:01 +0800

    adjust window size

--- makegrid.py
+++ makegrid.py
@@ -923,6 +923,7 @@ class getParms(QtGui.QWidget):
         self.layout.addWidget(self.scroll)
         self.setWindowTitle('SIREN - makegrid (' + fileVersion() + ') - Make resource grid file')
         self.center()
+        self.resize(int(self.sizeHint().width()* 1.07), int(self.sizeHint().height() * 1.07))
         self.show()
 
     def center(self):
