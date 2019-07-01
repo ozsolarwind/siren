bc380c92caf4b44f3a4d10ae555c7a8a638b178d
AnGus King <angus@ozsolarwind.com>
2019-07-01 09:19:32 +0800

    List of SIREN program files for packaging

---
+++ siren_files.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+source_files = ['check_siren.py', 'colours', 'credits', 'dijkstra_4',
+                'displayobject', 'displaytable', 'editini', 'floaters',
+                'getmap', 'getmerra2', 'grid', 'indexweather', 'inisyntax',
+                'makegrid', 'makerainfall2', 'makeweatherfiles', 'makeweather2',
+                'newstation', 'parents', 'plotweather', 'powerbalance2',
+                'powerclasses', 'powermodel', 'sammodels', 'samrun', 'senuser',
+                'siren', 'sirenicons', 'sirenm', 'sirenupd', 'ssc', 'station',
+                'superpower', 'towns', 'turbine', 'updateswis', 'viewresource',
+                'visualise', 'wascene', 'worldwindow', 'getfiles.ini', 'about.html',
+                'credits.html', 'help.html', 'makeweatherfiles.html', 'SIREN_notes.html',
+                'siren_versions.csv', 'siren_files.py', 'compare_to_siren.git.py']
+version_files = ['getmap', 'getmerra2', 'indexweather', 'makegrid',
+                 'makerainfall2', 'makeweatherfiles', 'makeweather2',
+                 'powerbalance2', 'siren', 'sirenm', 'sirenupd', 'updateswis']
