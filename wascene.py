d19015237596a011d55af5d844c07925d436d553
AnGus King <angus@ozsolarwind.com>
2021-04-20 16:03:41 +0800

    Dates on copyright statements

--- wascene.py
+++ wascene.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-#  Copyright (C) 2015-2020 Sustainable Energy Now Inc., Angus King
+#  Copyright (C) 2015-2021 Sustainable Energy Now Inc., Angus King
 #
 #  wascene.py - This file is part of SIREN.
 #
