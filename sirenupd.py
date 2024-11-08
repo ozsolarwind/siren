4495f8ad95ef8433e4e484bf2a4c43578e7c5aa8
Angus King <ozsolarwind@iinet.net.au>
2024-11-08 21:55:22 +0800

    Handle Windows and non-Windows environment better

--- sirenupd.py
+++ sirenupd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-#  Copyright (C) 2017-2023 Sustainable Energy Now Inc., Angus King
+#  Copyright (C) 2017-2024 Sustainable Energy Now Inc., Angus King
 #
 #  sirenupd.py - This file is part of SIREN.
 #
@@ -140,8 +140,18 @@ class UpdDialog(QtWidgets.QDialog):
             try:
                 pid = subprocess.Popen(command, stderr=subprocess.PIPE)
             except:
-                command[0] = command[0] + '.exe'
-                pid = subprocess.Popen(command, stderr=subprocess.PIPE)
+                if sys.platform == 'win32' or sys.platform == 'cygwin':
+                    try:
+                        command[0] = command[0] + '.exe'
+                        pid = subprocess.Popen(command, stderr=subprocess.PIPE)
+                    except:
+                        newgrid.addWidget(QtWidgets.QLabel(f'Error invoking {command[0]}\n\n' + \
+                                          response), row, 0, 1, 4)
+                        return
+                else:
+                    newgrid.addWidget(QtWidgets.QLabel(f'Error invoking {command[0]}\n\n' + \
+                                      response), row, 0, 1, 4)
+                    return
             response = get_response(pid.communicate()[1])
         row = 0
         if response != '200 OK':
